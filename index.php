4be430eb72075d8823cd214ee39cac682d9c3db9
pkarnati2004 <pkarnati@mit.edu>
2018-10-20 18:08:02 -0400

    Barebones website layout

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $pageTitle = 'MIT Global Startup Workshop 2019';
+    $metaDescription = 'MIT Global Startup Workshop is an annual conference that aims to accelerate entrepreneurial ecosystems around the world, through workshops, mentorship and skills training.'; 
+    $specificKeywords = 'entrepreneurial experience';
+    // $headerMainPageImageSrc = 'img/headers/header1.jpg';
+    $headerTitle = 'MIT Global Startup Workshop';
+    $headerSubTitle = 'About MIT GSW';
+    $classIndexMenu = 'active';
+// include('templates/nav-bar.php');
+    include('templates/landing-page.php');
+?>
+<!-- <br /><br />
+<?php
+    include('templates/footer.php');
+?> -->
---
+++ templates/landing-page.php
@@ -0,0 +1,9 @@
+<?php
+    // Increase this version every time you change the style.css file to bust cached css
+    $cssVersion = "1.0.9";
+?>
+
+
+<div>
+    <h1> My First Heading  </h1>
+</div>
