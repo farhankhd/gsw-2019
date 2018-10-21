1a55d5dd1df8135e1a9b94b39037893a4f4f4dec
pkarnati2004 <pkarnati@mit.edu>
2018-10-21 16:29:17 -0400

    adding landing page as background image

--- templates/landing-page.php
+++ templates/landing-page.php
@@ -1,16 +1,44 @@
-<?php
-    // Increase this version every time you change the style.css file to bust cached css
-    $cssVersion = "1.0.9";
-?>
-
+<!DOCTYPE HTML>
 <html>
+	<head>
+		<title>GSW 2019</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+    </head>
     
-<div>
+	<body class="landing-page-img">
+			<div>
 
-    <h1> GSW 2019</h1>
-    <img src="assets/landing-page/landing_page_white.png" />
+					<!-- <section id="banner">
 
-</div>
+						<div class="inner">
+                            <h1>MIT GSW 2019 </h1>
+                            <h1>Conference Location</h1>
+                            <h1>Dates will be announced in</h1>
+						</div>
+                    </section> -->
+                    <div class="inner">
+                            <h1>MIT GSW 2019 </h1>
+                            <h1>Conference Location</h1>
+                            <h1>Dates will be announced in</h1>
+					</div>
 
+            </div>
+            
+            <!-- <section id="one" class="landing-page">
+                <h1> HELLO </h1>
+			</section> -->
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
 </html>
-
