d2e1d88cf27a64b9fc127d2a8bae133f4ae55b7a
pkarnati2004 <pkarnati@mit.edu>
2018-11-02 10:41:56 -0400

    Adding blob to bottom

--- templates/landing-page.php
+++ templates/landing-page.php
@@ -30,60 +30,21 @@
                             <br>
                             <p>Conference Location</p>
                             <p>And Dates Will Be Announced In</p>
-                            <p id="demo"> </p>
+                            <p id="demo"></p>
                             <br>
-                            
                             <p style="font-size: 1vw"> <a style="color: white" href="http://gsw.mit.edu/2018/">Learn more about GSW 2018 in Thailand</a></p>
-
-                            <script>
-                                // Set the date we're counting down to
-                                var countDownDate = new Date("Jan 1, 2019 15:37:25").getTime();
-
-                                // Update the count down every 1 second
-                                var x = setInterval(function() {
-
-                                    // Get todays date and time
-                                    var now = new Date().getTime();
-                                    
-                                    // Find the distance between now and the count down date
-                                    var distance = countDownDate - now;
-                                    
-                                    // Time calculations for days, hours, minutes and seconds
-                                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                                    
-                                    // Output the result in an element with id="demo"
-                                    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-                                    + minutes + "m " + seconds + "s ";
-                                    
-                                    // If the count down is over, write some text 
-                                    if (distance < 0) {
-                                        clearInterval(x);
-                                        document.getElementById("demo").innerHTML = "EXPIRED";
-                                    }
-                                }, 1000);
-                                </script>
                         </div>
-                    </div>
-                    
-                    
+                    </div>  
             </section>
         </div>
-        
-		<!-- Scripts -->
-			<!-- <script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-            <script src="assets/js/main.js"></script> -->
+        <div class="blob-img">
+                <img src="../../images/landing-page/blob.png" />
+        </div>
             
-            <script src="lib/jquery/jquery.min.js"></script>
-            <script src="lib/jquery/jquery-migrate.min.js"></script>
-            <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/main.js"></script>
+        <script src="lib/jquery/jquery.min.js"></script>
+        <script src="lib/jquery/jquery-migrate.min.js"></script>
+        <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 	</body>
 </html>
