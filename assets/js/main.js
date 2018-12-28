4f8d0580d4d500bb42ffa3eacc5bedf857c4f003
pkarnati2004 <pkarnati@mit.edu>
2018-12-28 14:44:52 -0500

    Adding speaker scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,12 +29,56 @@ function countdown() {
     }, 1000);
 }
 
-function hidediv() {
-    document.getElementById("blob2").style.display = 'none';
+function getSpeakers() {
+    var filename = 'data/speakers.csv'
+    Papa.parse(filename, {
+        download: true,
+        header: true,
+        complete: function(results) {
+            console.log(results);
+
+            var speakers = results['data']
+            var rows = speakers.length
+            console.log(rows);
+
+            for (var i = 0; i < rows; i++) {
+                var currentSpeaker = speakers[i];
+                console.log(currentSpeaker);
+
+                var name = currentSpeaker['First Name'] + ' ' + currentSpeaker['Last Name'];
+
+                var anchor = name.replace(/ /g, "_");
+
+                var path = currentSpeaker['Filename'];
+                var bio = currentSpeaker['Bio'];
+                var title = currentSpeaker['Title'];
+
+                var associationMarkup = currentSpeaker['MIT association'] == 'Y' ?
+                    '<img class=\'speaker-picture-association\' src=\'images/speaker/mit.png\'/>' : '';
+
+                var imageMarkup = '<div class=\'speaker col-sm-6 col-md-3\'>' +
+                    associationMarkup +
+                    '<img class=\'team-picture\' src=\'images/speaker/' + path + '\'>';
+                // markup += imageMarkup + name + '</div>';
+
+                speakerInfoMarkup = '<div class=\'row speaker-expanded-bio\'>' +
+                    '<span class=\"anchor\" id=\"' + anchor + '\"></span>' +
+                    imageMarkup + '</div><div class=\'col-sm-6 col-md-9 speaker-expanded-text\'>' +
+                    '<div class=\'speaker-expanded-name\'>' + name + '</div>' +
+                    '<div class=\'speaker-expanded-position\'>' + title + '</div>' +
+                    bio + '</div></div>';
+
+                if (currentSpeaker['Keynote'] == 'Y') {
+                    $(speakerInfoMarkup).appendTo('#keynote-section-expanded');
+                } else {
+                    $(speakerInfoMarkup).appendTo('#speaker-section-expanded');
+                }
+            }
+
+        }
+    });
 }
 
-function showdiv() {
-    document.getElementById("blob2").style.display = 'block';
-}
+getSpeakers()
 
-countdown()
+// countdown()
--- speakers.php
+++ speakers.php
@@ -11,57 +11,33 @@
     </div>
 </div>
 
+<body>
+<div class="section" id="keynote-section-expanded">
+    <p class="section-header section-header-page" style="color: white">Keynotes</p>
+    <div class="row speaker-expanded-bio">
+        <div class="col-md-3"></div>
+        <div class="col-md-9"></div>
+    </div>
+</div>
+
+<div class="section" id="speaker-section-expanded">
+    <p class="section-header section-header-page" style="color: white">Speakers</p>
+    <div class="row speaker-expanded-bio">
+        <div class="col-md-3"></div>
+        <div class="col-md-9"></div>
+    </div>
+</div>
+
 <div class="section">
     <p class="endSpeakers">Come back soon, more speakers are added every day!</p>
 </div>
 
+<script src="assets/js/papaparse.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+
 <?php
-$scripts .= "
-<script src=\"splash/js/d3.js\"></script>
-<script>
-   d3.csv(\"data/speakers.csv\", function(error, speakers) {
-      if (error) throw error;
-
-    // columns for display on the site
-    var totalColumns = 4;
-    var rows = speakers.length/totalColumns;
-    for (var i = 0; i < rows; i++){
-        var markup = '<div class=\'row speaker-row text-center\'>';
-        for (var j = 0; j < totalColumns; j++){
-            var index = i*totalColumns + j;
-            if (index < speakers.length){
-                var speaker = speakers[i*totalColumns + j];
-
-                var name = speaker['First Name'] + ' ' + speaker['Last Name'];
-                var anchor = name.replace(/ /g,\"\");
-
-                var path = speaker['Filename'];
-                var bio = speaker['Bio'];
-                var title = speaker['Title'];
-                var associationMarkup = speaker['MIT association'] == 'Y' ?
-                    '<img class=\'speaker-picture-association\' src=\'speaker/img/mit.png\'/>' : '';
-
-                imageMarkup = '<div class=\'speaker col-sm-6 col-md-3\'>'
-                    + associationMarkup
-                    + '<img class=\'speaker-picture\' src=\'speaker/img/' + path + '\'>';
-                markup += imageMarkup + name + '</div>';
-
-                speakerInfoMarkup = '<div class=\'row speaker-expanded-bio\'>'
-                    + '<span class=\"anchor\" id=\"' + anchor + '\"></span>'
-                    + imageMarkup + '</div><div class=\'col-sm-6 col-md-9 speaker-expanded-text\'>'
-                    + '<div class=\'speaker-expanded-name\'>' + name + '</div>'
-                    + '<div class=\'speaker-expanded-position\'>' + title + '</div>'
-                    + bio + '</div></div>';
-                $(speakerInfoMarkup).appendTo('#speaker-section-expanded');
-            }
-            else {
-                markup += '<div class=\'speaker col-sm-6 col-md-3\'></div>';
-            }
-        }
-    }
-   
-   });
-</script>";
-
 include("templates/footer.php");
 ?>
+
+</body>
