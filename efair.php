1be98cafd4cd35dd421d87d62d3f44286eff6180
pkarnati2004 <pkarnati@mit.edu>
2019-04-24 19:31:28 -0400

    adding efair page

---
+++ efair.php
@@ -0,0 +1,87 @@
+<?php
+    $pageTitle = 'Entrepreneurship Fair | MIT Global Startup Workshop 2018';
+    include('templates/nav-bar.php');
+?>
+
+<div class="parallax-window header" data-parallax="scroll" style="background-image: url('headers/img/grand_header.jpg');">
+    <div class="header-overlay">
+    <h1 class="parallax-text">
+        ENTREPRENEURSHIP FAIR
+    </h1>
+    </div>
+</div>
+
+<body style="background-image: url('images/comp/comp-background.jpg');">
+
+<div class="section">
+    <div class="comp-center">
+        <h1> ENTREPRENEURSHIP FAIR</h1>
+        <h4> Connect with key players in the startup ecosystem and see what our BPC finalists are up to in this special 3-hour event </h4>
+        <br>
+        <div class="line"></div>
+        <br>
+        <div class="col-lg-12">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <p  class="white" style="text-align:left">MIT GSW’s Entrepreneurship Fair will feature startups, 
+                investors, incubators, accelerators, and other stakeholders in Colombia’s entrepreneurial 
+                ecosystem. This event is an incredible networking opportunity and it’s open to all GSW attendees. 
+                </p>
+                <p  class="white" style="text-align:left">The fair will consist of two sections: the <strong>Startup Showcase</strong> and the <strong>Expo.</strong> </p>
+                <!-- <p class="white text-center">Consulte nuestro Reglamento Oficial para más información.</p> -->
+            </div>
+            <div class="col-3"></div>
+        </div>
+        <br><br>
+
+        <br>
+        
+    </div>
+    <div class="col-lg-12 text-center">
+        <br>
+        <div class="line"></div>
+        
+        <br><br>
+    </div>
+    <div class="col-lg-12 text-center">
+        <div>
+            <div class="col-md-6 comp-content">
+                <h2> STARTUP SHOWCASE </h2>
+                <p> Talk to successful entrepreneurs one-on-one and learn about their startups at this event, 
+                which will showcase finalists from the MIT GSW Business Plan Competition. 
+                Learn more about our finalists soon! They will be updated here shortly.</p>
+                <br><br>
+            </div>
+            <div class="col-md-6 comp-content">
+                <h2> EXPO </h2>
+                <p>Meet and connect with key players across the Colombian entrepreneurial ecosystem. 
+                This event will feature various organizations, from startups, to investors, 
+                to incubators, and other stakeholders.
+                </p>
+                <p>If you represent a startups or organization related to entrepreneurship 
+                and you are interested in participating in the expo, there are still some unfilled booths. 
+                Please contact us at 
+                <a style="color: white; text-decoration: underline" href="mailto:gsw-contact@mit.edu"> gsw-contact@mit.edu</a> 
+                with the subject “Expo Participation Interest”. 
+                </p>
+                <br><br>
+            </div>
+        </div>
+        
+    </div>
+    <div class="row text-center">
+        
+        
+        
+    </div>
+</div>
+
+<script src="assets/js/papaparse.min.js"></script>
+<script src="assets/js/main.js?v=1.1"></script>
+<script src="assets/js/jquery.min.js"></script>
+
+<?php
+include("templates/footer.php");
+?>
+
+</body>
