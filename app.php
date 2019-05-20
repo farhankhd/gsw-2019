650f991f394d85fbe05049afb74ca8e274cac7c8
pkarnati2004 <pkarnati@mit.edu>
2019-05-20 17:45:53 -0400

    app.php

--- app.php
+++ app.php
@@ -20,10 +20,31 @@
 <div class="section">
     <div class="text-center">
         <h2> Conference App</h2>
-        <h4>Come back soon to learn more about the app</h4>
-        <div class="col-lg-12" style="max-width: 100%">
-            <img src='images/launch/iphone.png' style="max-width: 80%">
+        <div class='row vertical-align'>
+            <div class='col-md-6'>
+                <h4 style='text-align: justify'>Get our free app to: </h4>
+                <div class='row'>
+                    <h4><ul style='color: white; text-align: justify'>
+                        <li>Connect with speakers and other attendees
+                        <li>Access the schedule and speaker profiles</li>
+                        <li>Navigate the venue using an interactive map</li>
+                        <li>Vote for Startup Showcase winner</li>
+                        <li>Receive live announcements</li>
+                    </ul></h4>
+                </div>
+            </div>
+            <div class='col-md-6'>
+                <div class='col-md-6'>
+                    <a href='https://itunes.apple.com/us/app/mit-gsw-2019/id1458106305?ls=1&mt=8'><img class='app-logo' src='images/logo/apple.svg'></a>
+                </div>
+                <div class='col-md-6'>
+                    <a href='https://play.google.com/store/apps/details?id=edu.mit.eeeh6p'><img class='app-logo' src='images/logo/google.png'></a>
+                </div>
+            </div>
+        
         </div>
+        
+    
     </div>
 </div>
 <br>
